f45590163f6e35028426fd0aeba16fe8b4732a56
hadirr <hadir.elayeb@esprit.tn>
2024-02-29 00:57:13 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEvaluationComponent } from './components/add-evaluation/add-evaluation.component';
+import { EvaluationComponent } from './components/evaluation/evaluation.component';
+import { HomeComponent } from './components/home/home.component';
+import { DadminComponent } from './components/dadmin/dadmin.component';
+import { AddPublicationComponent } from './components/publicationn/add-publication/add-publication.component';
+import { SpecialiteComponent } from './components/specialite/specialite.component';
+import { AddSpecialiterComponent } from './components/add-specialiter/add-specialiter.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { ProfscheduleComponent } from './components/profschedule/profschedule.component';
+import { SessionComponent } from './session/session.component';
+import { ModifscheduleComponent } from './components/schedule/modifschedule/modifschedule.component';
+import { EmploiComponent } from './components/schedule/emploi/emploi.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+
+  { path: '', component: HomeComponent }, // Assurez-vous que HomeComponent est importé
+  // Autres routes...
+
+  { path: 'evaluation', component: EvaluationComponent },
+  { path: 'add-evaluation', component: AddEvaluationComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'prof', component: ProfscheduleComponent },
+  { path: 'ss', component: SessionComponent },
+  { path: 'dadmin', component: DadminComponent },
+  { path: 'add-publication', component: AddPublicationComponent },
+  { path: 'specialite', component: SpecialiteComponent },
+  { path: 'add-specialiter', component: AddSpecialiterComponent },
+  { path: 'modifS/:id', component: ModifscheduleComponent },
+  { path: 'emp/:id', component: EmploiComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,93 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CoursComponent } from './components/cours/cours.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { EvaluationComponent } from './components/evaluation/evaluation.component';
+import { ClasseComponent } from './components/classe/classe.component';
+import { SpecialiteComponent } from './components/specialite/specialite.component';
+import { PublicationComponent } from './components/publication/publication.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddEvaluationComponent } from './components/add-evaluation/add-evaluation.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { DadminComponent } from './components/dadmin/dadmin.component';
+import { AddPublicationComponent } from './components/publicationn/add-publication/add-publication.component';
+import { AddSpecialiterComponent } from './components/add-specialiter/add-specialiter.component';
+import { DashbordlatbarComponent } from './components/dashbordlatbar/dashbordlatbar.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ProfscheduleComponent } from './components/profschedule/profschedule.component';
+import { SessionComponent } from './session/session.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ModifscheduleComponent } from './components/schedule/modifschedule/modifschedule.component';
+import { EmploiComponent } from './components/schedule/emploi/emploi.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    
+    CoursComponent,
+    ScheduleComponent,
+    EvaluationComponent,
+    ClasseComponent,
+    SpecialiteComponent,
+    PublicationComponent,
+    RegisterComponent,
+    AddEvaluationComponent,
+    DadminComponent,
+    AddPublicationComponent,
+    AddSpecialiterComponent,
+    DashbordlatbarComponent,
+    ProfscheduleComponent,
+    SessionComponent,
+    ModifscheduleComponent,
+    EmploiComponent,
+
+
+
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    InputTextModule,
+ 
+    ButtonModule,
+    FormsModule,
+    HttpClientModule
+
+
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
