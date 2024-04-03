0fc290d7661584b8311f0dd5f2d86baee4ad9f45
oussema-kossentini <oussema.kossentini@esprit.tn>
2024-04-03 23:12:45 +0100

    jasser+oussema+oumaimav1

--- src/main/java/tn/esprit/spring/entities/Role.java
+++ src/main/java/tn/esprit/spring/entities/Role.java
@@ -3,5 +3,7 @@ package tn.esprit.spring.entities;
 public enum Role {
     ADMINISTRATOR,
     PROFESSOR,
+    USER,
+    TEACHER,
     STUDENT
 }
--- src/main/java/tn/esprit/spring/repositories/UserRepository.java
+++ src/main/java/tn/esprit/spring/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package tn.esprit.spring.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import tn.esprit.spring.entities.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
+    @Query("{'email' : ?0}")
+    Optional<User> findByEmail(String email);
+
 }
