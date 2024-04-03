5c8937b19fef31ff76a12ab36f87d393d7d1e585
oussema-kossentini <oussema.kossentini@esprit.tn>
2024-04-03 23:28:52 +0100

    jasser+oussema+oumaima

--- src/main/java/tn/esprit/spring/entities/Publication.java
+++ src/main/java/tn/esprit/spring/entities/Publication.java
@@ -1,16 +1,21 @@
 package tn.esprit.spring.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jdk.jfr.ContentType;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.CascadeType;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import java.io.File;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
 @Getter
@@ -19,17 +24,28 @@ import java.util.Set;
 @NoArgsConstructor
 @Document
 @FieldDefaults(level = AccessLevel.PRIVATE)
-
+@JsonInclude(value= JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Publication implements Serializable {
     @Id
     String idPublication;
+    String title;
     String content;
-    Date creationDate;
-    boolean like;
-    @DBRef
-    private User user;
+    String imageToken; // Champ pour stocker le token JWT de l'image
+    private Date creationDate;
+    int likes;
+    String fileName;
+    private int shareCount; // Nombre de fois que la publication a été partagée
+
 
     @DBRef
+    private User creator;
+
+    @DBRef
+    @JsonIgnore
     private Set<Comment> comments;
+    @DBRef
+    private List<User> likedByUsers;
+
 
 }
